9780e849845019d48f1a2a80b3c07450367b559c
ArturTukaev <80099400+ArturTukaev@users.noreply.github.com>
2021-12-06 16:56:59 +0500

    САМОСТОЯТЕЛЬНАЯ РАБОТА № 4

---
+++ bd_film/delete.php
@@ -0,0 +1,18 @@
+<?php
+ define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'films');
+
+$mysql1 = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($mysql1 ->connect_errno) exit ('Ошибка');
+$mysql1->set_charset('utf8');
+$induction = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+$result = mysqli_query($induction, "SELECT id FROM films");
+
+$zapros="DELETE FROM films WHERE id=" . $_GET['id'];
+ mysqli_query($induction, $zapros);
+ header("Location: index.php");
+ exit;
+?>
---
+++ bd_film/save_edit.php
@@ -0,0 +1,32 @@
+<html> <body>
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'films');
+
+$mysql1 = @new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+if ($mysql ->connect_errno) exit ('Ошибка');
+$mysql1->set_charset('utf8');
+
+$mysql1->close();
+
+$induction = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+$result = mysqli_query($induction, "SELECT id, name, genre, director, year, box_office FROM films");
+
+$films = mysqli_fetch_assoc($result);
+//print_r($users);
+ $zapros="UPDATE films SET name='".$_GET['name'].
+"', genre='".$_GET['genre']."', director='"
+.$_GET['director']."', year='".$_GET['year'].
+"', box_office='".$_GET['box_office']."' WHERE id="
+.$_GET['id'];
+ mysqli_query($induction, $zapros);
+ if (mysqli_affected_rows($induction)>0) {
+ echo 'Все сохранено. <a href="index.php"> Вернуться к списку
+фильмов </a>'; }
+ else { echo 'Ошибка сохранения. <a href="index.php">
+Вернуться к списку фильмов</a> '; }
+?>
+</body> </html>
